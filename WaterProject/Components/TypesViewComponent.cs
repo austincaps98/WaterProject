caa825d450ce250bd3f3c8acfe225864571a34c9
Austin Caprio <austincaps98@gmail.com>
2022-02-23 17:07:15 -0700

    push to origin

---
+++ WaterProject/Components/TypesViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WaterProject.Models;
+
+namespace WaterProject.Components
+{
+    public class TypesViewComponent : ViewComponent
+    {
+        private IWaterProjectRepository repo { get; set; }
+
+        public TypesViewComponent (IWaterProjectRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var types = repo.Projects
+                .Select(x => x.ProjectType)
+                .Distinct()
+                .OrderBy(x => x);
+
+            return View(types);
+        }
+    }
+}
