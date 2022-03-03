73dbc7b7c679633e7a21cbb5f752f0110ccc6f1d
Austin Caprio <austincaps98@gmail.com>
2022-03-02 22:30:04 -0700

    P5 : V1

---
+++ WaterProject/Models/SessionBasket.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Http;
+using WaterProject.Infrastructure;
+
+namespace WaterProject.Models
+{
+    public class SessionBasket : Basket
+    {
+        [JsonIgnore]
+        public ISession Session { get; set; }
+
+        public override void AddItem(Project proj, int qty)
+        {
+            base.AddItem(proj, qty);
+            Session.SetJson("Basket", this);
+        }
+
+        public override void RemoveItem(Project proj)
+        {
+            base.RemoveItem(proj);
+            Session.SetJson("Basket", this);
+        }
+
+        public override void ClearBasket()
+        {
+            base.ClearBasket();
+            Session.Remove("Basket");
+        }
+    }
+}
